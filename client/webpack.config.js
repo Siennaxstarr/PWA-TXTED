ffc5c0c244ae6170888861b2f2e9252d3534a791
Siennaxstarr <siennastarrv@gmail.com>
2024-03-13 14:01:19 -0800

    set up configuration of webpack file for html, service worker, and  manifest plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,8 +17,39 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    // customize the webpack build process 
     plugins: [
-      
+     // generates a HTML file or allows you to supply your own HTML file
+      new HtmlWebpackPlugin({
+        // supplied my own index.html file and told it where its located
+        template: './index.html'
+      }),
+      // plugin for the manifest.json file to generate sizing and fingerprints
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+      // creates a service worker file for you or allows you to use your own that you already have
+      new InjectManifest({
+        // grab the service worker file from the current directory
+        swSrc: './src-sw.js', 
+        // generates a new service worker file inside the dist folder 
+        swDest: 'src-sw.js'
+      })
     ],
 
     module: {
